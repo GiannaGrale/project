ae9f6bfef8706d88c5ba26cdf58c195a4c65c090
Anna <giannagrale@gmail.com>
2021-02-06 17:08:59 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+"use strict";
+//alert (3);
+//const result = confirm("Are you here?");
+//console.log(result);
+
+//const answer = +prompt ("18 years?", "");
+//console.log (typeof(answer));
+
+const answers = [];
+
+//answers[0]= prompt("Your name?","");
+//answers[1]= prompt ("Your age?","");
+
+//document.write(answers);
+//console.log (typeof(answers));
+const page = ("hey");
+console.log (`http:link`${hey});
